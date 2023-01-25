efe2fedbff73fed4af3d654b53aed95f0eec295f
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2023-01-25 22:09:50 +0200

    adding final styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,19 @@
 import { Component } from 'react';
-import './App.css';
 import { Searchbar } from '../Searchbar/Searchbar';
 import { ImageGallery } from '../ImageGallery/ImageGallery';
 import { Loader } from '../Loader/Loader';
+
+import css from './App.module.css';
 
 export class App extends Component {
   state = {};
   render() {
     return (
-      <>
+      <div className={css.app}>
         <Searchbar></Searchbar>
         <ImageGallery></ImageGallery>
         <Loader></Loader>
-      </>
+      </div>
     );
   }
 }
